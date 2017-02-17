94cefbf211166e59caebb635e3a21e6b52d83157
Dave Touretzky <dst@cs.cmu.edu>
2017-02-17 03:16:16 -0500

    Renamed jviewer

--- cozmo_fsm/__init__.py
+++ cozmo_fsm/__init__.py
@@ -3,5 +3,5 @@ from .transitions import *
 from .program import *
 from .trace import tracefsm
 from .particle import *
-from .jviewer import ParticleViewer
+from .particle_viewer import ParticleViewer
 from .cozmo_kin import *
