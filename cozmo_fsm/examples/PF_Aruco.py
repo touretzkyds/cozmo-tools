2318d470b35199ae7d2caaedc62eda22f71480dc
Dave Touretzky <dst@cs.cmu.edu>
2017-02-17 02:59:01 -0500

    Update to program.py

---
+++ cozmo_fsm/examples/PF_Aruco.py
@@ -0,0 +1,34 @@
+"""
+PF_Aruco demonstrates a particle filter using ArUco markers.
+There are three sensor models provided:
+  ArucoDistanceSensorModel -- distances only
+  ArucoBearingSensorModel -- bearings only
+  ArucoCombinedSensorModel -- combined distances + bearings
+
+In the particle viewer window:
+   the WASD keys move the robot
+  'e' forces an evaluation step
+  'r' forces a resampling
+  'v' displays the weight statistics
+  'z' re-randomizes the particles.
+"""
+
+from cozmo_fsm import *
+from cozmo.util import degrees, Pose
+
+class PF_Aruco(StateMachineProgram):
+    def start(self):
+        landmarks = {
+            0 : Pose(-55, 160, 0, angle_z=degrees(90)),
+            1 : Pose( 55, 160, 0, angle_z=degrees(90)),
+            2 : Pose(160,  55, 0, angle_z=degrees( 0)),
+            3 : Pose(160, -55, 0, angle_z=degrees( 0))
+        }
+        pf = ParticleFilter(robot,
+                            landmarks = landmarks,
+                            sensor_model = ArucoCombinedSensorModel(robot))
+        self.particle_filter = pf
+        super().start()
+        v = ParticleViewer(robot)
+        v.startThread()
+        
