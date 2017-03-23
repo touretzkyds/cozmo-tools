bd32c35c343825819f82cdd42079a1ec993e24aa
Dave Touretzky <dst@cs.cmu.edu>
2017-03-23 02:29:03 -0400

    Added phrase substitution

--- cozmo_fsm/speech.py
+++ cozmo_fsm/speech.py
@@ -1,5 +1,5 @@
 try:
-  import speech_recognition as sr
+    import speech_recognition as sr
 except: paass      
 
 from threading import Thread
@@ -9,19 +9,62 @@ from .events import SpeechEvent
 
 class Thesaurus():
     def __init__(self):
-        self.entries = dict()
+        self.words = dict()
         self.add_homophones('cozmo', \
                             ["cozimo","cosimo","cosmo", \
                              "kozmo","cosmos","cozmos"])
+        self.add_homophones('right', ['write','wright'])
+        self.add_homophones('1',['one','won'])
+        self.add_homophones('cube1',['q1','coupon','cuban'])
+        self.phrase_tree = dict()
+        self.add_phrases('cube1',['cube 1'])
+        self.add_phrases('cube2',['cube 2'])
+        self.add_phrases('cube3',['cube 3'])
+        self.add_phrases('paperclip',['paper clip'])
+        self.add_phrases('deli-slicer',['deli slicer'])
+        
 
     def add_homophones(self,word,homophones):
         if not isinstance(homophones,list):
             homophones = [homophones]
         for h in homophones:
-            self.entries[h] = word
+            self.words[h] = word
 
-    def lookup(self,word):
-        return self.entries.get(word,word)
+    def lookup_word(self,word):
+        return self.words.get(word,word)
+
+    def add_phrases(self,word,phrases):
+        if not isinstance(phrases,list):
+            phrases = [phrases]
+        for phrase in phrases:
+            wdict = self.phrase_tree
+            for pword in phrase.split(' '):
+                wdict[pword] = wdict.get(pword,dict())
+                wdict = wdict[pword]
+            wdict[''] = word
+
+    def substitute_phrases(self,words):
+        result = []
+        while words != []:
+            word = words[0]
+            del words[0]
+            wdict = self.phrase_tree.get(word,None) 
+            if wdict is None:
+                result.append(word)
+                continue
+            prefix = [word]
+            while words != []:
+                wdict2 = wdict.get(words[0],None)
+                if wdict2 is None: break
+                prefix.append(words[0])
+                del words[0]
+                wdict = wdict2
+            subst = wdict.get('',None)
+            if subst is not None:
+              result.append(subst)
+            else:
+              result = result + prefix
+        return result
 
 class SpeechListener():
   def __init__(self,robot,thesaurus):
@@ -35,15 +78,15 @@ class SpeechListener():
           audio = self.rec.listen(source)
           try:
               utterance = self.rec.recognize_google(audio).lower()
-              words = [self.thesaurus.lookup(w) for w in utterance.split(" ")]
+              words = [self.thesaurus.lookup_word(w) for w in utterance.split(" ")]
+              words = self.thesaurus.substitute_phrases(words)
               string = " ".join(words)
               print("Heard: '%s'" % string)
               evt = SpeechEvent(string,words)
               self.robot.erouter.post(evt)
-          except sr.UnknownValueError:
-              pass
           except sr.RequestError as e:
               print("Could not request results form google speech recognition service; {0}".format(e)) 
+          except: pass
 
   def start(self):
     self.thread = Thread(target=self.speech_listener)
