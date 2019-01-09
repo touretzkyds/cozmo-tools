8389919f9d5533f660eea7382f901b8f011d4a8a
Dave Touretzky <dst@cs.cmu.edu>
2019-01-08 22:53:11 -0500

    Devel

--- cozmo_fsm/opengl.py
+++ cozmo_fsm/opengl.py
@@ -25,7 +25,7 @@ def init():
     if not INIT_DONE:
         INIT_DONE = True
         glutInit()
-        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH)
+        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_ALPHA)
 
         # Killing window should not directly kill main program
         glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_CONTINUE_EXECUTION)
