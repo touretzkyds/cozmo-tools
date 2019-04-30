d04c49831fbdcdc40432c3ed34d939883b69742d
Dave Touretzky <dst@cs.cmu.edu>
2019-04-30 05:18:55 -0400

    typo

--- cozmo_fsm/particle.py
+++ cozmo_fsm/particle.py
@@ -1112,7 +1112,7 @@ class SLAMParticleFilter(ParticleFilter):
         good_markers = []
         for marker in seen_marker_objects.values():
             if marker.id_string in self.robot.world.world_map.objects or \
-               self.candidate_arucos.get(marker.id_string,-1) > 5:
+               self.sensor_model.candidate_arucos.get(marker.id_string,-1) > 5:
                 good_markers.append(marker.id)
         walls = self.sensor_model.generate_walls_from_markers(seen_marker_objects, good_markers)
         for wall in walls:
